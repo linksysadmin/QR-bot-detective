fd8670b21b283679e2fc9aa5d46660a25949f5c2
inside <linksysadmin@proton.me>
2024-03-03 11:37:31 +0200

    v=0.2

--- run.sh
+++ run.sh
@@ -4,8 +4,13 @@ then
     echo "Python не установлен. Установите Python и запустите скрипт снова."
     exit
 fi
+if [ -d ".venv" ]
+then
+    echo "Запуск QR Bot"
+else
+    echo "Установка QR Bot"
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
 
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
 python3 main.py
