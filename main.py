32e11ddfcb99bfa7f9678998d488b47850c1ba92
inside <linksysadmin@proton.me>
2024-03-03 09:48:29 +0200

    v=0.2

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ import db
 logger = logging.getLogger(__name__)
 
 bot = telebot.TeleBot(BOT_TOKEN)
-IMAGE = "images/image.jpg"
+FILE_IMAGE = "images/image.jpg"
 
 
 def handle_photo(file):
-    with open(IMAGE, 'wb') as new_file:
+    with open(FILE_IMAGE, 'wb') as new_file:
         new_file.write(file)
     try:
-        image = cv2.imread(IMAGE)
+        image = cv2.imread(FILE_IMAGE)
         decoded_objects = decode(image)
 
         if decoded_objects:
